5ff41fae2ba6e6899dec2d4fd8e1c4c3b6acc0c5
Sergey <scadgek@live.com>
2019-02-28 20:23:25 +0200

    add id to photo

--- src/main/java/rainbow.ponies/Photo.java
+++ src/main/java/rainbow.ponies/Photo.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 public class Photo
 {
+  private final int id;
   private final boolean isVertical;
   private final List<String> tags;
 
-  public Photo( boolean isVertical, List<String> tags )
+  public Photo( int id, boolean isVertical, List<String> tags )
   {
+    this.id = id;
     this.isVertical = isVertical;
     this.tags = tags;
+  }
+
+  public int getId()
+  {
+    return id;
   }
 
   public boolean isVertical()
