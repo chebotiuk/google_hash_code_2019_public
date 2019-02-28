c6ca11dab07085f9ed50083a0f050908100439c0
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-02-28 20:21:02 +0200

    Points counter signature

---
+++ src/main/java/rainbow.ponies/PointsCounter.java
@@ -0,0 +1,15 @@
+package rainbow.ponies;
+
+public class PointsCounter {
+
+    long countPoints (Slideshow slideshow) {// TODO test
+
+
+        return 1l;
+    }
+
+    long countPoints (Slide s1, Slide s2) {
+        return 1;
+        // TODO test
+    }
+}
